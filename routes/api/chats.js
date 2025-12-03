50652a01425e6e6d58652a112fc48bb2fdd5dce1
Tsuyakashi <kashakdaniil@gmail.com>
2025-12-03 19:51:51 +0300

    refactor: add /chats prefix to get and delete chat routes

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -5,7 +5,7 @@ const chatController = require('../../controllers/chatController')
 router.get('/', chatController.getAllChats.bind(chatController));
 router.post('/chats', chatController.createChat.bind(chatController));
 router.post('/chat/:id/send', chatController.sendToChat.bind(chatController));
-router.get('/:id', chatController.getChatHistory.bind(chatController));
-router.delete('/:id', chatController.deleteChat.bind(chatController));
+router.get('/chats/:id', chatController.getChatHistory.bind(chatController));
+router.delete('/chats/:id', chatController.deleteChat.bind(chatController));
 
 module.exports = router;
